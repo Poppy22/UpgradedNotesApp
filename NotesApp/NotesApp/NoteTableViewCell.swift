ac5b34cee9b4f1326376df476ee1e628eaf9e77a
Carmen Popa <carmen.popa@mready.net>
2018-04-01 23:49:18 +0300

    Can delete multiple notes by selecting them

--- NotesApp/NotesApp/NoteTableViewCell.swift
+++ NotesApp/NotesApp/NoteTableViewCell.swift
@@ -8,30 +8,55 @@
 
 import UIKit
 
+protocol NoteCellDelegate {
+    func onCellLongTap(longPressgestureRecognizer: UILongPressGestureRecognizer, cell: UITableViewCell)
+}
+
 class NoteTableViewCell: UITableViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var shouldDeleteButton: UIButton!
+    @IBOutlet weak var checkButtonWidth: NSLayoutConstraint!
     
-
+    var delegate: NoteCellDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setGestureRecognizer()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
-    func loadCell(note: NoteClass, isSelected: Bool) {
+    func loadCell(note: NoteClass, isDeleteModeOn: Bool, isSelected: Bool) {
         self.titleLabel.text = note.title
         self.descriptionLabel.text = note.detail
-        shouldDeleteButton.isHidden = !isSelected
         
+        if !isDeleteModeOn {
+            self.checkButtonWidth.constant = 0
+        } else {
+            self.checkButtonWidth.constant = 55
+        }
+        if !isSelected {
+            shouldDeleteButton.setImage(UIImage(named: "checkbox_off.png"), for: .normal)
+            //shouldDeleteButton.image = UIImage(named: "checkbox_off.png")
+        } else {
+            shouldDeleteButton.setImage(UIImage(named: "checkbox_on.png"), for: .normal)
+        }
+    }
+    
+    func setGestureRecognizer() {
+        let longPressGesture:UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(self.handleLongPress(_:)))
+        longPressGesture.minimumPressDuration = 1.0
+        longPressGesture.delegate = self as UIGestureRecognizerDelegate
+        self.addGestureRecognizer(longPressGesture)
+    }
+    
+    @objc func handleLongPress(_ longPressGestureRecognizer: UILongPressGestureRecognizer) {
+        delegate?.onCellLongTap(longPressgestureRecognizer: longPressGestureRecognizer, cell: self)
     }
 
 }
--- NotesApp/NotesApp/TableNotesViewController.swift
+++ NotesApp/NotesApp/TableNotesViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 
 let NoteIdentifier = "noteCell"
+let SegueToNote = "segueToNote"
+let SegueToLogin = "segueToLogin"
 
-class TableNotesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class TableNotesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NoteCellDelegate {
     
     @IBOutlet weak var deleteModeBarButton: UIBarButtonItem!
     @IBOutlet weak var loginBarButton: UIBarButtonItem!
@@ -19,12 +21,15 @@ class TableNotesViewController: UIViewController, UITableViewDelegate, UITableVi
     @IBOutlet weak var notesTableView: UITableView!
     
     var tableData = [NoteClass]()
+    var deleteModeOn = false
+    var selectedNotesIndex = [Int]()
     
     override func viewWillAppear(_ animated: Bool) {
         loadInitialSettings()
     }
     
     override func viewDidLoad() {
+        populateTableWithMockData()
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -36,17 +41,25 @@ class TableNotesViewController: UIViewController, UITableViewDelegate, UITableVi
     }
     
     func loadInitialSettings() {
-        populateTableWithMockData()
+        
         //check if there are any notes
         noNotesView.isHidden = !(tableData.count == 0)
         notesTableView.isHidden = (tableData.count == 0)
-        deleteModeBarButton.image = nil
+        
+        //the delete mode is not set yet
+        if deleteModeOn {
+            deleteModeBarButton.image = UIImage(named: "ic_nav_close.png")
+        } else {
+            deleteModeBarButton.image = nil
+        }
+        self.title = ""
+        loginBarButton.image = UIImage(named: "ic_nav_profile.png")
     }
     
     func populateTableWithMockData() {
         for i in 1...20 {
             let newNote = NoteClass()
-            newNote.set(title: "Title " + String(i), detail: "Description " + String(i), images: [], id: "10" + String(i), lastUpdate: 100)
+            newNote.set(title: "Title " + String(i) + " only one line in length", detail: "A very very very very very long Description " + String(i), images: [], id: "10" + String(i), lastUpdate: 100)
             tableData.append(newNote)
         }
     }
@@ -58,18 +71,73 @@ class TableNotesViewController: UIViewController, UITableViewDelegate, UITableVi
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: NoteIdentifier, for: indexPath as IndexPath) as! NoteTableViewCell
         let note = tableData[indexPath.row]
-        cell.loadCell(note: note, isSelected: false)
+        cell.delegate = self
+        cell.loadCell(note: note, isDeleteModeOn: deleteModeOn, isSelected: selectedNotesIndex.contains(indexPath.row))
         return cell
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if !deleteModeOn {
+            let note = tableData[indexPath.row]
+            self.performSegue(withIdentifier: SegueToNote, sender: note)
+        } else {
+            if selectedNotesIndex.contains(indexPath.row) {
+                let index = selectedNotesIndex.index(of: indexPath.row)
+                selectedNotesIndex.remove(at: index!)
+            } else {
+                selectedNotesIndex.insert(indexPath.row, at: 0)
+            }
+            notesTableView.reloadData()
+            self.title = String(selectedNotesIndex.count)
+        }
     }
-    */
+    
+    //long press on a cell to enter delete mode
+    func onCellLongTap(longPressgestureRecognizer: UILongPressGestureRecognizer, cell: UITableViewCell) {
+        if longPressgestureRecognizer.state == UIGestureRecognizerState.began {
+            
+            if let indexPath = notesTableView.indexPath(for: cell) {
+                loginBarButton.image = #imageLiteral(resourceName: "ic_trash")
+                deleteModeBarButton.image = #imageLiteral(resourceName: "ic_close_popup")
+                deleteModeOn = true
+                selectedNotesIndex.append(indexPath.row)
+                notesTableView.reloadData()
+                self.title = String(selectedNotesIndex.count)
+            }
+        }
+    }
 
+    func deleteSelectedNotes(alert: UIAlertAction) {
+        selectedNotesIndex.sort()
+        selectedNotesIndex.reverse()
+        
+        for noteIndex in selectedNotesIndex {
+            let index = self.selectedNotesIndex.index(of: noteIndex)
+            selectedNotesIndex.remove(at: index!)
+            self.tableData.remove(at: noteIndex)
+        }
+        deleteModeOn = false
+        loadInitialSettings()
+        notesTableView.reloadData()
+    }
+
+    @IBAction func exitDeleteMode(_ sender: Any) {
+        deleteModeOn = false
+        selectedNotesIndex.removeAll()
+        loadInitialSettings()
+        notesTableView.reloadData()
+    }
+    
+    @IBAction func didTapOnRightBarButton(_ sender: Any) {
+        if !deleteModeOn {
+            self.performSegue(withIdentifier: SegueToLogin, sender: loginBarButton)
+        } else {
+            let alertController = UIAlertController(title: nil, message: "Are you sure you want to permanently delete these notes?", preferredStyle: .alert)
+            let defaultActionCancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+            let defaultActionDelete = UIAlertAction(title: "Delete", style: .default, handler: deleteSelectedNotes)
+            alertController.addAction(defaultActionCancel)
+            alertController.addAction(defaultActionDelete)
+            present(alertController, animated: true, completion: nil)
+        }
+    }
 }
